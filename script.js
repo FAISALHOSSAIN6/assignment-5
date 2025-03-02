6b5830ecb0d487129db42f1a0412922a6b35c013
FAISALHOSSAIN6 <faisal012345677@gmail.com>
2025-03-02 03:30:18 -0800

    added comment style

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-let elements =  document.getElementsByClassName('complete-btn');
+let elements = document.getElementsByClassName('complete-btn');
 
-for(let element of elements){
-   element.addEventListener('click', function(event){
-      
+for (let element of elements) {
+   element.addEventListener('click', function (event) {
+
       alert('Board Update Successfully')
-    
-      
-    element.disabled = true;
-   //  -------
-      
-     const cardTitle = event.target.parentNode.parentNode.parentNode.querySelector('.card-title').innerText;
-   
-   const commentContainer = document.getElementById('comment-box');
-   // document.getElementById('comment-box').style= 8  ;
-   let commentDiv = document.createElement('div');
-   // document.createElement('div').style.margin = '8';
-   commentDiv.innerText = 'You have completed the task ' + cardTitle + ' 1:32:34 AM';
-      
-   commentContainer.appendChild(commentDiv).style.margin= '5px';
-  
 
-// --------------
-   const number = document.getElementById('num').innerText;
 
-   const convertedNumber = parseInt(number) - 1;
-   
-   document.getElementById('num').innerText = convertedNumber;
+      element.disabled = true;
+      //  -------
 
-// -------------------------------
-if(parseInt(convertedNumber) < 1){
-   alert(" Congratulation, You have completed the task successfully ")
-}
+      const cardTitle = event.target.parentNode.parentNode.parentNode.querySelector('.card-title').innerText;
 
-// ------
+      const commentContainer = document.getElementById('comment-box');
+      // document.getElementById('comment-box').style.marginTop= '4px'  ;
+      let commentDiv = document.createElement('div');
 
-   const mainNumber = document.getElementById('main-num').innerText;
+      commentDiv.style.marginTop = '10px'
+      commentDiv.style.backgroundColor = 'lightgray'
+      commentDiv.style.borderRadius = '10px'
+      commentDiv.innerText = 'You have completed the task ' + cardTitle + ' 1:32:34 AM';
 
-   const convertedMainNumber = parseInt(mainNumber) + 1 ;
+      commentContainer.appendChild(commentDiv);
 
-   document.getElementById('main-num').innerText =convertedMainNumber;
 
-      
-      
+      // --------------
+      const number = document.getElementById('num').innerText;
+
+      const convertedNumber = parseInt(number) - 1;
+
+      document.getElementById('num').innerText = convertedNumber;
+
+      // -------------------------------
+      if (parseInt(convertedNumber) < 1) {
+         alert(" Congratulation, You have completed the task successfully ")
+      }
+
+      // ------
+
+      const mainNumber = document.getElementById('main-num').innerText;
+
+      const convertedMainNumber = parseInt(mainNumber) + 1;
+
+      document.getElementById('main-num').innerText = convertedMainNumber;
+
+
+
    })
-    
+
 }
 
-document.getElementById('new-today').addEventListener('click',function(){
+document.getElementById('new-today').addEventListener('click', function () {
    window.location.href = "question.html"
 })
 
 
 // ------------------
-document.getElementById('clear-btn').addEventListener('click', function(){
-   document.getElementById('comment-box').innerText = "" ;
+document.getElementById('clear-btn').addEventListener('click', function () {
+   document.getElementById('comment-box').innerText = "";
 })
 
 
-const dateName = ['Sat','Sun', 'Mon','Tue', 'Wed','Thu','Fri',]
+const dateName = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri',]
 
-const monthName = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+const monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 let newDate = new Date();
 
 let currentDate = newDate.getDate();
 
-let currentDateName = dateName[parseInt(newDate.getDate()) - 1 ];
+let currentDateName = dateName[parseInt(newDate.getDate()) - 1];
 
-let currentMonth = monthName[newDate.getMonth() ] ;
+let currentMonth = monthName[newDate.getMonth()];
 
 
 
@@ -76,17 +79,17 @@ let currentYear = newDate.getFullYear();
 document.getElementById('date-name').innerText = currentDateName;
 
 
-const date =document.getElementById('date') ;
+const date = document.getElementById('date');
 
 date.innerText = currentMonth + '  ' + currentDate + ' ' + currentYear
 
 // ---------------------------
 
 
-document.getElementById('color-btn').addEventListener('click', function(){
+document.getElementById('color-btn').addEventListener('click', function () {
    const randomColor = '#' + Math.floor(Math.random() * 1666678)
-   
+
    document.body.style.backgroundColor = randomColor;
-   
-   
+
+
 })
